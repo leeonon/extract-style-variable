a89d064b6f14bd3bea655fced2286b35d405dd80
leeonon <v5sollee@gmail.com>
2023-02-20 00:03:57 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export const a = '123';
+// https://github.com/postcss/postcss/blob/main/docs/README-cn.md
+// https://juejin.cn/post/6844903761463214087
+
+import type { ChildNode } from 'postcss';
+
+import fs from 'node:fs';
+import path from 'node:path';
+import postcss from 'postcss';
+
+type Result = Map<
+	string,
+	Array<{
+		prop: string;
+		value: string;
+	}>
+>;
+
+const src = path.resolve('./test/styles/index.css');
+const css = fs.readFileSync(src);
+const ast = postcss.parse(css);
+const result: Result = new Map();
+
+ast.walk((decl, index) => {
+	eachNodes(decl, result);
+});
+
+console.log('🚀 ~ file: index.ts:12 ~ ast.walk ~ result', result);
+
+function eachNodes(node: ChildNode, result: Result) {
+	if (node.type === 'rule') {
+		node.nodes.forEach((child) => {
+			if (child.type === 'decl') {
+				const key = node.selectors.join('-');
+				const value = { prop: child.prop, value: child.value };
+				result.set(
+					key,
+					result.get(key) ? result.get(key)?.concat(value) || [] : [value]
+				);
+				eachNodes(child, result);
+			}
+		});
+	}
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,13 @@
-import type { Options } from 'tsup';
+// https://github.com/stevancorre/psychowiki-bot/blob/30381d2389/tsup.config.dev.ts
+// https://jsmanifest.com/create-a-modern-typescript-javascript-library/
+import { defineConfig } from 'tsup';
 
-export const tsup: Options = {
+export default defineConfig({
   entry: ["src/index.ts"],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: true,
-  clean: true
-}
+  clean: true,
+  sourcemap: true,
+  onSuccess: "node dist/index.cjs"
+})
